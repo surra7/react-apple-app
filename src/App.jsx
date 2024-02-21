2f65c1ae29f5aec447c035009de84daff73a9077
surra7 <subum6302@gmail.com>
2024-02-21 17:20:54 +0900

    ~movie 모달창

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import styled from "styled-components";
 import "./App.css";
 import Nav from "./componemts/Nav";
 import Banner from "./componemts/Banner";
+import Row from "./componemts/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AC"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </Container>
   );
 }
